9ba71299b0c7b8cef5de9b54ae7141d1d91990ed
retrostreams <retrostreams@users.noreply.github.com>
2017-07-09 01:18:48 +0200

    get rid of j9.u.f.BiFunctions

--- src/main/java/java9/util/function/BiFunction.java
+++ src/main/java/java9/util/function/BiFunction.java
@@ -25,6 +25,7 @@
 package java9.util.function;
 
 import java9.util.function.Function;
+import java9.util.Objects;
 
 /**
  * Represents a function that accepts two arguments and produces a result.
@@ -50,4 +51,22 @@ public interface BiFunction<T, U, R> {
      * @return the function result
      */
     R apply(T t, U u);
+
+    /**
+     * Returns a composed function that first applies this function to
+     * its input, and then applies the {@code after} function to the result.
+     * If evaluation of either function throws an exception, it is relayed to
+     * the caller of the composed function.
+     *
+     * @param <V> the type of output of the {@code after} function, and of the
+     *           composed function
+     * @param after the function to apply after this function is applied
+     * @return a composed function that first applies this function and then
+     * applies the {@code after} function
+     * @throws NullPointerException if after is null
+     */
+    default <V> BiFunction<T, U, V> andThen(Function<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return (T t, U u) -> after.apply(apply(t, u));
+    }
 }
