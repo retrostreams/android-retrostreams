0841bcbfb57017a949300c3449e114d45e47f078
retrostreams <retrostreams@users.noreply.github.com>
2017-07-09 11:57:49 +0200

    get rid of j9.u.f.UnaryOperators

--- src/main/java/java9/util/function/UnaryOperator.java
+++ src/main/java/java9/util/function/UnaryOperator.java
@@ -41,4 +41,13 @@ import java9.util.function.Function;
  */
 public interface UnaryOperator<T> extends Function<T, T> {
 
+    /**
+     * Returns a unary operator that always returns its input argument.
+     *
+     * @param <T> the type of the input and output of the operator
+     * @return a unary operator that always returns its input argument
+     */
+    static <T> UnaryOperator<T> identity() {
+        return t -> t;
+    }
 }
